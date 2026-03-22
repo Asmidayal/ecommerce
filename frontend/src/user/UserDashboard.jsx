f9d4d80cf322f64abb575b42e23e33b7e2b812d6
Asmi <ami.work015@gmail.com>
2026-03-22 13:56:00 +0530

    added overlay for better UI

--- frontend/src/user/UserDashboard.jsx
+++ frontend/src/user/UserDashboard.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../UserStyles/UserDashboard.css'
 import { useNavigate } from 'react-router'
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import '../features/user/userSlice';
+import { logout,removeSuccess } from '../features/user/userSlice';
 function UserDashboard  ({user})  {
+    const dispatch=useDispatch();
     const navigate=useNavigate();
+    const[menuVisible,setMenuVisible]=useState(false);
+    function toggleMenu(){
+        setMenuVisible(!menuVisible);
+    }
     const options=[
         {name:'Orders',funcName:orders},
           {name:'Account',funcName:profile},
-            {name:'LogOut',funcName:logout}
+            {name:'LogOut',funcName:logoutUser}
     ]
     if(user.role==='admin'){
         options.unshift({
@@ -20,25 +29,37 @@ function UserDashboard  ({user})  {
      function profile(){
     navigate('/profile')
     }
-     function logout(){
-    console.log('logout')
+     function logoutUser(){
+    dispatch(logout())
+    .unwrap()//converting action to promise
+    .then(()=>{
+        toast.success('Logout successful',{autoClose:3000})
+        dispatch(removeSuccess())
+        navigate('/login')
+    })
+    .catch((error)=>{
+         toast.success(error.message||'Logout failed',{autoClose:3000})
+    })
     }
     function dashboard(){
         navigate('admin/dashboard')
     }
   return (
+    <>
+    <div className={`overlay ${menuVisible?'show':''}`}>onClick={toggleMenu}</div>
     <div className='dashboard-container'>
-        <div className='profile-header'>
+        <div className='profile-header' onClick={toggleMenu}>
             <img src={user.avatar.url?user.avatar.url:'./images/sun.jpeg'} alt='Profile Picture' className='profile-avatar'></img>
             <span className='profile-name'> {user.name||'User'}</span>
         </div>
-    <div className='menu-options'>
+  { menuVisible &&( <div className='menu-options'>
         {options.map((item)=>(
         <button key={item.name}className='menu-option-btn'onClick={item.funcName}>{item.name}</button>
 ))}
-    </div>
+    </div>)}
       
     </div>
+    </>
   )
 }
 
