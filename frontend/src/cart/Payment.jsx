d20b047234a21fa5b49ff663ef8cefe0c50b5e49
Asmi <ami.work015@gmail.com>
2026-04-12 16:04:56 +0530

    checkout page

--- frontend/src/cart/Payment.jsx
+++ frontend/src/cart/Payment.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import PageTitle from '../components/PageTitle'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import CheckoutPath from './CheckoutPath'
+import '../CartStyles/Payment.css'
+import { Link } from 'react-router-dom'
 
 const Payment = () => {
+    const orderItem=JSON.parse(sessionStorage.getItem('orderItem'));
   return (
-    <div>
-      
-    </div>
+  <>
+  <PageTitle title="Payment"/>
+  <Navbar/>
+  <CheckoutPath activePath={2}/>
+  <div className='payment-container'>
+   <Link to='/order/confirm' className='payment-go-back'>Proceed to Payment</Link> 
+   <button className='payment-btn'>Pay({orderItem.total})/-</button>
+  </div>
+  <Footer/>
+  </>
   )
 }
 
