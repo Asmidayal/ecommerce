a65cb0fda186501ecf4c7843bcc50ce7ce984665
Asmi <ami.work015@gmail.com>
2026-06-22 17:01:58 +0530

    accessing order info

--- frontend/src/Orders/MyOrders.jsx
+++ frontend/src/Orders/MyOrders.jsx
@@ -5,13 +5,31 @@ import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
 import { Link } from 'react-router-dom'
 import { LaunchOutlined } from '@mui/icons-material'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllOrders } from '../features/order/orderSlice'
+import { toast } from 'react-toastify'
+import { removeErrors } from '../features/order/orderSlice'
+import Loader from '../components/Loader'
+
 
 const MyOrders = () => {
+  const{orders,loading,error}=useSelector(state=>state.order);
+  console.log(orders);
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    if(error){
+       toast.error(error,{autoClose:3000});
+                  dispatch(removeErrors());
+    }
+  dispatch(getAllOrders());
+  },[dispatch,error]);
+
   return (
     <>
     <Navbar/>
     <PageTitle title="My Orders"/>
-    <div className='my-orders-container'>
+   { loading?(<Loader/>):orders.length>0?(<div className='my-orders-container'>
       <h1>My Orders</h1>
       <div className='table-responsive'>
         <table className='orders-table'>
@@ -24,18 +42,26 @@ const MyOrders = () => {
             <th>View Order</th>
             </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>1111</td>
-                <td>5</td>
-                <td>Processing</td>
-                <td>400</td>
-                <td><Link to="/order/:id"><LaunchOutlined/></Link></td>
+         <tbody>
+             { orders.map((order)=>(
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.orderItems.length}</td>
+                <td>{order.orderStatus}</td>
+                <td>{order.totalPrice}</td>
+                <td><Link to={`/order/${order._id}`} className='order-link'><LaunchOutlined/></Link></td>
               </tr>
+             
+             )) }
             </tbody>
             </table>
             </div>
-    </div>
+    </div>):(
+      <div className='no-orders'>
+        <p className='no-order-message'>You have no orders yet.</p>
+        
+      </div>
+    )}
     <Footer/>
     </>
   )
