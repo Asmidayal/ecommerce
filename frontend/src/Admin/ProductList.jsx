a9c7a2718c5033a5586bd381f08a74bb0f8608d1
Asmi <ami.work015@gmail.com>
2026-07-05 23:53:21 +0530

    Poduct list

--- frontend/src/Admin/ProductList.jsx
+++ frontend/src/Admin/ProductList.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../AdminStyles/ProductsList.css'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
+import { Link } from 'react-router-dom'
+import { Edit, Delete } from '@mui/icons-material'
 
 const ProductList = () => {
   return (
@@ -26,15 +28,18 @@ const ProductList = () => {
                     </thead>
                     <tbody>
                         <tr>
-                         <td></td>
-                         <td></td>
-                         <td></td>
-                        <td></td>
-                        <td></td>
-                         <td></td>
-                        <td></td>
-                        <td></td>
-                         <td></td>
+                         <td>1</td>
+                         <td><img src="" alt="Product Image"/></td>
+                         <td>Lipstick</td>
+                        <td>300/-</td>
+                        <td>4.5</td>
+                         <td>Lips</td>
+                         <td>4</td>
+                        <td>5-06-2026</td>
+                         <td>
+                            <Link to ='/admin/products/:productId' className='action-icon edit-icon'><Edit/></Link>
+                               <Link to ='/admin/products/:productId' className='action-icon delete-icon'><Delete/></Link>
+                         </td>
                         </tr>
                     </tbody>
                 </table>
