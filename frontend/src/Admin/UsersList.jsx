1965a67f6e3ffb2bd0363510e6869953dd28b268
Asmi <ami.work015@gmail.com>
2026-07-26 18:55:05 +0530

    edit and delete icon

--- frontend/src/Admin/UsersList.jsx
+++ frontend/src/Admin/UsersList.jsx
@@ -3,6 +3,8 @@ import '../AdminStyles/UsersList.css'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/PageTitle'
 import Footer from '../components/Footer'
+import { Link } from 'react-router'
+import {Delete , Edit } from '@mui/icons-material'
 
 const UsersList = () => {
   return (
